b0da4a741cae14c04c5caa9211546c549860f6cd
witty-collection-3981 <yisroel96@gmail.com>
2018-11-22 00:18:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    return "NO, NOT SINCE 1938!"
+  # elsif phrase.upcase
+  #  return "NO, NOT SINCE 1938!"
   elsif phrase == phrase.upcase
     return  "NO, NOT SINCE 1938!"
   end
