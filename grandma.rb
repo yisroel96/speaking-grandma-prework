47393f5fe328edcf7ba798a8143c8a85789099e7
witty-collection-3981 <yisroel96@gmail.com>
2018-11-22 00:23:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU Grandma!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
